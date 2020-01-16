36d4d525ea51d5b1ca468b2b9c949ad8472efcf8
DEV-WH\wangyi <wangyi@douyu.tv>
2020-01-16 10:18:34 +0800

    algorithm add

--- src/main/java/com/wy/algorithm/string/RegularExpression.java
+++ src/main/java/com/wy/algorithm/string/RegularExpression.java
@@ -19,7 +19,6 @@ public class RegularExpression {
             return false;
         }
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-
         dp[0][0] = true;
 
         for (int i = 0; i < p.length(); i++) {
